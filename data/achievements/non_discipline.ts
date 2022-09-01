25212a0e545809c999c7325e3c50204a3ab7cfed
이대형 <apm5018@naver.com>
2022-09-02 00:59:29 +0900

    사업성과 - 비교과활동 페이지 개발

---
+++ data/achievements/non_discipline.ts
@@ -0,0 +1,40 @@
+export const data = [
+    {
+        images: [
+            '/non_discipline/포항 소동리 풍력단지 견학1.jpg',
+            '/non_discipline/포항 소동리 풍력단지 견학2.jpg',
+            '/non_discipline/포항 소동리 풍력단지 견학3.jpg',
+        ],
+        title: '[포항시 북구 소동리 ENT 풍력발전단지 탐방]',
+        visit_date: '탐방 날짜 : 2021년 11월 16일 화요일',
+        details: [
+            '- 풍력에 관심있는 한동대 재학생들과 풍력단지 건설현장 방문',
+            '- 소형 수평축 3-블레이드 풍력타워 구조 알아보기'
+        ]
+    },
+    {
+        images: [
+            '/non_discipline/경주 풍력발전단지 탐방1.jpg',
+            '/non_discipline/경주 풍력발전단지 탐방2.jpg',
+            '/non_discipline/경주 풍력발전단지 탐방3.jpg',
+        ],
+        title: '[경주 풍력발전단지 탐방]',
+        visit_date: '탐방 날짜 : 2021년 12월 17일 금요일',
+        details: [
+            '- 풍력 산업단지에 방문하여 현장을 이해하고 현장문제를 논문연구와 연결',
+            '- 수혜 대학원생의 풍력 관련 산업체 인식 제고'
+        ]
+    },
+    {
+        images: [
+            '/non_discipline/단기 집중강좌1.jpg',
+            '/non_discipline/단기 집중강좌2.jpg',
+            '/non_discipline/단기 집중강좌3.jpg',
+        ],
+        title: '[에너지시스템 제어를 위한 강화학습 기술 단기 집중강좌]',
+        visit_date: '교육 날짜 : 2021년 12월 06일 ~ 12월 15일 (온라인, 오프라인 병행)',
+        details: [
+            '- 모델 불확실성이 큰 풍력발전시스템 운용 및 관리 기술 개발을 위한 최신 인공지능 제어기술의 활용 능력 확보',
+        ]
+    },
+]
--- pages/achievements/non-discipline.tsx
+++ pages/achievements/non-discipline.tsx
@@ -1,10 +1,57 @@
 import type { NextPage } from 'next'
-import { Box } from '@mui/material'
+import Image from 'next/image'
+import { Box, Grid, Typography } from '@mui/material'
+import PageTitleBar from '../../components/common/PageTitleBar'
+import WindPowerOutlinedIcon from '@mui/icons-material/WindPowerOutlined';
+import { data } from '../../data/achievements/non_discipline'
+import React from 'react';
+
+const styles = {
+    container: {
+        background: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1))`,
+        padding: '0px 0px 0 40px',
+        marginBottom: '80px',
+    }
+}
 
 const NonDisciplinePage: NextPage = () => {
     return (
         <Box style={{ padding: '50px 10% 50px 10%' }}>
-            
+            <PageTitleBar title={'1차년도 비교과활동'} icon={WindPowerOutlinedIcon} />
+            <Box sx={{ padding: "0px 20px" }}>
+                {
+                    data.map((item, index) => (
+                        <React.Fragment key={index}>
+                            <Grid container display="flex" justifyContent="center" alignItems="center" sx={{ marginBottom: '20px' }}>
+                                {
+                                    item.images.map((image, imageId) => (
+                                        <Grid key={imageId} item md={3}>
+                                            <Image width={250} height={200} src={image} />
+                                        </Grid>
+                                    ))
+                                }
+                            </Grid>
+                            <Box style={styles.container}>
+                                <Grid container justifyContent="space-between">
+                                    <Grid item sx={{ marginTop: '30px', paddingRight: '40px', width: '600px'}}>
+                                        <Typography textAlign="left" variant="h5">{item.title}</Typography>
+                                        <Typography textAlign="left" variant="h6" fontWeight="400">{item.visit_date}</Typography>
+                                        <br />
+                                        {
+                                            item.details.map((detail, detailId) => (
+                                                <Typography key={detailId} textAlign="left" fontWeight="400">{detail}</Typography>
+                                            ))
+                                        }
+                                    </Grid>
+                                    <Grid item>
+                                        <Image width={350} height={200} src={`/non_discipline/discipline_background_crop.png`} />
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </React.Fragment>
+                    ))
+                }
+            </Box>
         </Box>
     )
 }
