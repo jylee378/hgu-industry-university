37d61769b2e84fcc0373d9b2b46c36e12bf806c3
이대형 <apm5018@naver.com>
2022-09-02 22:52:17 +0900

    타이틀 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Power Lab</title>
+        <title>THE WIND BLOWS</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Image src={`/landing.jpg`} width="1500px" height="650px" layout="responsive" alt="landingImage" />
