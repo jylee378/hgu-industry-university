2fa7ad0b3354fcd20bbc89a8aee55c2b0d90efa3
이대형 <apm5018@naver.com>
2022-09-18 23:36:52 +0900

    왼쪽 홈이동 로고 화면 클때 벌어지는 문제 수정

--- components/LeftMenu.tsx
+++ components/LeftMenu.tsx
@@ -12,8 +12,8 @@ const LeftMenu = (props: leftMenuProps) => {
     const router = useRouter()
 
     return (
-        <Grid container justifyContent='flex-start' alignItems='center' style={{ padding: !props.isResponsive ? '0px 0px 0px 20px' : '5px 20px' }}>
-            <Grid item lg={2} md={4}>
+        <Grid container spacing={3} justifyContent='flex-start' alignItems='center' style={{ padding: !props.isResponsive ? '0px 0px 0px 20px' : '5px 20px' }}>
+            <Grid item>
                 <a href='https://handong.edu/' target="_blank" rel="noreferrer">
                     <Image src={`/logo.jpeg`} width={!props.isResponsive ? 200 : 140} height={!props.isResponsive ? 70 : 50} alt="logo" />
                 </a>
@@ -21,7 +21,7 @@ const LeftMenu = (props: leftMenuProps) => {
             {/* 메인 페이지 아닐 경우에 메인으로 이동하는 로고가 표시된다 */}
             {
                 router.pathname !== "/" && (
-                    <Grid item lg={3} md={4} xs={3} style={{ minWidth: 150, fontSize: 18, fontWeight: 'bold' }}>
+                    <Grid item style={{ fontSize: 18, fontWeight: 'bold' }}>
                         <Typography><Link href={`/`}>THE WIND BLOWS</Link></Typography>
                     </Grid>
                 )
